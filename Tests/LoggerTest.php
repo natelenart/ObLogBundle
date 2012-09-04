0bf12eacdd4464249ffad5d2e4cf717799a6efa7
marcaube <marc@tourismecie.com>
2012-09-04 12:36:30 -0400

    prepare bundle to receive tests

---
+++ Tests/LoggerTest.php
@@ -0,0 +1,17 @@
+<?php
+namespace Ob\LogBundle\Tests;
+
+/**
+ * Unit tests for LogBundle
+ */
+class LoggerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Log a visit
+     */
+    public function testCompleteScenario()
+    {
+        // TODO: add basic unit tests
+        $this->assertTrue(1 === 1);
+    }
+}
