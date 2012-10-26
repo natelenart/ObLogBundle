553e9f2d04cc11902844f6762e332aa36f2d898c
Nate Lenart <nlenart@ambrygen.com>
2012-10-25 18:11:30 -0700

    Refactored Logger

--- Logging/Logger.php
+++ Logging/Logger.php
@@ -50,28 +50,67 @@ class Logger
      */
     public function logEvent(&$entity, $type = 'visit', $data = null)
     {
-        // Get the id value
+        // Get the object id from the entity
+        $objectId = $this->getObjectIdFromEntity($entity);
+
+        // Grab a new Event instance
+        $event = $this->generateEvent();
+
+        // Attach data to the event
+        $event
+            ->setObjectClass(get_class($entity))
+            ->setObjectId($objectId)
+            ->setType($type)
+            ->setEnv($this->env)
+            ->setData($data)
+            ;
+
+        // Save the event in the database
+        $this->save($event);
+    }
+
+    /**
+     * Use reflection to extract the entity's id
+     *
+     * @param object $entity
+     * @return mixed The object id
+     */
+    protected function getObjectIdFromEntity(&$entity)
+    {
         $meta = $this->em->getClassMetadata(get_class($entity));
         $identifierField = $meta->getSingleIdentifierFieldName();
         $objectId = $meta->getReflectionProperty($identifierField)->getValue($entity);
+        return $objectId;
+    }
 
-        // Ensure event class implements EventInterface
+    /**
+     * Generate a new event of the dependency-injected type
+     *
+     * @return mixed The generated event
+     */
+    protected function generateEvent()
+    {
+        // Generate a new event
         $event_class = $this->event_class;
         $event = new $event_class;
+
+        // Ensure event supports the methods we need
         if ( ! $event instanceof Entity\EventInterface) {
           throw new InvalidArgumentException( // see the "use" statement for this at the top of the file
             sprintf('Event class "%s" must implement Ob\LogBundle\Entity\EventInterface', $event_class)
           );
         }
 
-        // Create a new event (click/visit/print/whatever)
-        $event->setObjectClass(get_class($entity))
-            ->setObjectId($objectId)
-            ->setType($type)
-            ->setEnv($this->env)
-            ->setData($data);
+        return $event;
+    }
 
-        // Save event in database
+    /**
+     * Save the event in the database
+     *
+     * @param object $event
+     */
+    protected function save($event)
+    {
         $this->em->persist($event);
         $this->em->flush();
     }
